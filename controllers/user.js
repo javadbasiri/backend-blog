d39f92546e2ccf96a79f3b47c7e0b69a36e9e080
javad basiri <javadbasiri.js@gmail.com>
2024-10-14 22:12:21 +0330

    get user

--- controllers/user.js
+++ controllers/user.js
@@ -2,18 +2,10 @@ const UserModel = require("../models/User");
 
 exports.getUser = async (req, res) => {
   try {
-    const { query } = req;
-
-    if (!query.email) return res.status(400).send("email is required!");
-
-    const user = await UserModel.findOne({ email: query.email });
-
-    if (!user) return res.status(400).send("user is not registered!");
-
-    res.send(user);
+    if (!req.user) throw new Error("user not found!");
+    res.send(req.user);
   } catch (error) {
     console.log(error);
-    
     res.status(500).send(error.message);
   }
 };
@@ -22,9 +14,9 @@ exports.getAllUser = async (req, res) => {
   const users = await UserModel.find().lean();
   if (!users || !users.length) return res.send("there is no registered user!");
 
-  users.forEach(user => {
-    Reflect.deleteProperty(user, 'password')
+  users.forEach((user) => {
+    Reflect.deleteProperty(user, "password");
   });
 
-  res.send(users)
+  res.send(users);
 };
